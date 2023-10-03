4f0059dc025e6990e2baa6807add8cf331e33fa4
Ivan <you@example.com>
2023-10-03 20:47:00 +0300

    first commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿string OutProgression(int n){
+    string s = "";
+    if (n == 1) return s + "1";
+    else {
+        return n + ", " + OutProgression(n - 1);
+    }
+}
+
+int GetNumber(string message){
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+
+System.Console.WriteLine(OutProgression(GetNumber("Введите число: ")));
---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int GetNumber(string message){
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int SumMtoN(int m, int n){
+    if (m == n) return m;
+    else return n + SumMtoN(m, n-1);
+}
+
+System.Console.WriteLine(SumMtoN(GetNumber("Введите первое число: "),
+                                    GetNumber("Введите первое число: ")));
